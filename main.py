457a04b3a91cb73019c494d4b93c75e4e1780942
Riccardo Cereghino <riccardo.cereghino@gmail.com>
2021-08-27 16:26:43 +0200

    -

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ Confronta σ2M con 2 ||A||2F/M.
 def MonteCarloTrace(matrix, M):
     X = [0]
 
-    for m in range(1, M):
+    for m in range(1, M + 1):
         # Rademacher
         u = []
         for uu in range(0, len(matrix)):
@@ -28,10 +28,15 @@ def MonteCarloTrace(matrix, M):
         X.append(X[m - 1] + (X_m - X[m - 1]) / m)
 
     sigma_squared = 0
-    for m in range(1, M):
-        sigma_squared += (X[m] - X[M - 1]) ** 2 / (M - 1)
+    for m in range(1, M + 1):
+        sigma_squared += (X[m] - X[M]) ** 2 / (M - 1)
 
-    return X[M - 1], sigma_squared
+    ss = 0
+    for m in range(1, M + 1):
+        ss += (X[m] - X[M]) ** 2
+    sss = ss / M - 1
+    ssss = ss / M
+    return X[M], sigma_squared
 
 
 # Press the green button in the gutter to run the script.
@@ -81,13 +86,13 @@ if __name__ == '__main__':
         var_100.append(var)
 
     print("Varianza stimata con M=(5): {}, frobenius: {}, delta: {}, trace: {}".format(
-        sum(var_5) / 100, frobenius / 5, sum(var_5) / 100 - frobenius / 5, sum(trace_5) / 100))
+        var_5[0], frobenius / 5, var_5[0] - frobenius / 5, sum(trace_5) / 100))
     print("Varianza stimata con M=(10): {}, frobenius: {}, delta: {}, trace: {}".format(
-        sum(var_10) / 100, frobenius / 10, sum(var_10) / 100 - frobenius / 10, sum(trace_10) / 100))
+        var_10[0] / 100, frobenius / 10, var_10[0] / 100 - frobenius / 10, sum(trace_10) / 100))
     print("Varianza stimata con M=(25): {}, frobenius: {}, delta: {}, trace: {}".format(
-        sum(var_25) / 100, frobenius / 25, sum(var_25) / 100 - frobenius / 25, sum(trace_25) / 100))
+        var_25[0] / 100, frobenius / 25, var_25[0] / 100 - frobenius / 25, sum(trace_25) / 100))
     print("Varianza stimata con M=(100): {}, frobenius: {}, delta: {}, trace: {}".format(
-        sum(var_100) / 100, frobenius / 100, sum(var_100) / 100 - frobenius / 100, sum(trace_100) / 100))
+        var_100[0] / 100, frobenius / 100, var_100[0] / 100 - frobenius / 100, sum(trace_100) / 100))
 
     weights = np.ones_like(trace_100) / float(len(trace_100))
 
@@ -98,11 +103,22 @@ if __name__ == '__main__':
     plt.axvline(tr + np.sqrt(var_5[0]), color='k', linestyle='dashed', linewidth=1)
     plt.show()
 
+    plt.title("5 var")
+    plt.hist(var_5, weights=weights, color='#E52B50', edgecolor='#FBCEB1', alpha=0.65, bins=25)
+    plt.axvline(frobenius / 5, color='k', linestyle='dashed', linewidth=1)
+    plt.show()
+
+
     plt.title("10")
     plt.hist(trace_10, weights=weights, color='#E52B50', edgecolor='#FBCEB1', alpha=0.65)
     plt.axvline(tr, color='k', linestyle='dashed', linewidth=1)
     plt.axvline(tr - np.sqrt(var_10[0]), color='k', linestyle='dashed', linewidth=1)
     plt.axvline(tr + np.sqrt(var_10[0]), color='k', linestyle='dashed', linewidth=1)
+    plt.show()
+
+    plt.title("10 var")
+    plt.hist(var_10, weights=weights, color='#E52B50', edgecolor='#FBCEB1', alpha=0.65, bins=25)
+    plt.axvline(frobenius / 10, color='k', linestyle='dashed', linewidth=1)
     plt.show()
 
     plt.title("25")
@@ -112,10 +128,20 @@ if __name__ == '__main__':
     plt.axvline(tr + np.sqrt(var_25[0]), color='k', linestyle='dashed', linewidth=1)
     plt.show()
 
+    plt.title("25 var")
+    plt.hist(var_25, weights=weights, color='#E52B50', edgecolor='#FBCEB1', alpha=0.65, bins=25)
+    plt.axvline(frobenius / 25, color='k', linestyle='dashed', linewidth=1)
+    plt.show()
+
     plt.title("100")
     plt.hist(trace_100, weights=weights, color='#E52B50', edgecolor='#FBCEB1', alpha=0.65)
     plt.axvline(tr, color='k', linestyle='dashed', linewidth=1)
     plt.axvline(tr - np.sqrt(var_100[0]), color='k', linestyle='dashed', linewidth=1)
     plt.axvline(tr + np.sqrt(var_100[0]), color='k', linestyle='dashed', linewidth=1)
-
     plt.show()
+
+    plt.title("100 var")
+    plt.hist(var_100, weights=weights, color='#E52B50', edgecolor='#FBCEB1', alpha=0.65, bins=25)
+    plt.axvline(frobenius / 100, color='k', linestyle='dashed', linewidth=1)
+    plt.show()
+
